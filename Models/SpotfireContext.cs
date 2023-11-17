7909097329ebac62825cd72159ede9d7ac3b7b74
Dursun Dashdamirov <dursun.dashdemirov.std@bhos.edu.az>
2023-11-17 09:23:43 +0400

    remove connection string

--- Models/SpotfireContext.cs
+++ Models/SpotfireContext.cs
@@ -23,7 +23,7 @@ namespace ORRFINALL.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server= yalquzaq.database.windows.net,1433; Database = Zangazur; User ID = yalquzaq@yalquzaq; Password = Hakaton2022;Trusted_Connection =False;Integrated Security=False");
+                //optionsBuilder.UseSqlServer("your_db_ConnectionString");
             }
         }
         //jdbc:sqlserver://yalquzaq.database.windows.net:1433;database=Zangazur;user=yalquzaq@yalquzaq;password=Hakaton2022
